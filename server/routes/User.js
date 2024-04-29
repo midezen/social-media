2b1ff345a69394620381dbc7383e78467e506aaf
midezen <victordiya90@gmail.com>
2024-04-29 14:53:29 +0100

    added user routes

--- server/routes/User.js
+++ server/routes/User.js
@@ -1,10 +1,23 @@
 import express from "express";
 import { verifyToken } from "../verifyToken.js";
-import { deleteUser, updateUser } from "../controllers/User.js";
+import {
+  deleteUser,
+  getUser,
+  likePost,
+  savePost,
+  unlikePost,
+  unsavePost,
+  updateUser,
+} from "../controllers/User.js";
 
 const router = express.Router();
 
 router.put("/:id", verifyToken, updateUser);
 router.delete("/:id", verifyToken, deleteUser);
+router.get("/:id", verifyToken, getUser);
+router.put("/likePost/:id", verifyToken, likePost);
+router.put("/unlikePost/:id", verifyToken, unlikePost);
+router.put("/savePost/:id", verifyToken, savePost);
+router.put("/unsavePost/:id", verifyToken, unsavePost);
 
 export default router;
