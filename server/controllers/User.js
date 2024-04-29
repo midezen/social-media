214355fac0fa875f282676e20ead79aeb91c6bb6
midezen <victordiya90@gmail.com>
2024-04-29 14:48:12 +0100

    created getUser, likePost, unlikePost, savePost, unsavePost user controllers

--- server/controllers/User.js
+++ server/controllers/User.js
@@ -1,3 +1,4 @@
+import Post from "../models/Post.js";
 import User from "../models/User.js";
 
 export const updateUser = async (req, res) => {
@@ -38,5 +39,54 @@ export const deleteUser = async (req, res) => {
     }
   } else {
     res.status(403).json("Permission denied");
+  }
+};
+
+export const getUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+export const likePost = async (req, res) => {
+  try {
+    await Post.findById(req.params.id, { $push: { likes: req.user.id } });
+    res.status(200).json("successfully liked post");
+  } catch (err) {
+    res.status(200).json(err);
+  }
+};
+
+export const unlikePost = async (req, res) => {
+  try {
+    await Post.findById(req.params.id, { $pull: { likes: req.user.id } });
+    res.status(200).json("successfully unliked post");
+  } catch (err) {
+    res.status(200).json(err);
+  }
+};
+
+export const savePost = async (req, res) => {
+  try {
+    await User.findById(req.user.id, {
+      $push: { savedPosts: req.params.id },
+    });
+    res.status(200).json("Post saved successfully");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+export const unsavePost = async (req, res) => {
+  try {
+    await User.findById(req.user.id, {
+      $pull: { savedPosts: req.params.id },
+    });
+    res.status(200).json("successfully unsaved post");
+  } catch (err) {
+    res.status(500).json(err);
   }
 };
