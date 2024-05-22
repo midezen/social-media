ea8702d32dc066097e513eed449e2f7bd4fe1fad
midezen <victordiya90@gmail.com>
2024-05-22 15:26:08 +0100

    created getMessages and deleteMessage functions in Message controller

--- server/controllers/Message.js
+++ server/controllers/Message.js
@@ -20,6 +20,33 @@ export const editMessage = async (req, res) => {
         { new: true }
       );
       res.status(200).json(editedMessage);
+    } else {
+      return res.status(403).json("You can only edit your own messages");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+export const getMessages = async (req, res) => {
+  try {
+    const messages = await Message.find({ conversationId: req.params.id }).sort(
+      { createdAt: "asc" }
+    );
+    res.status(200).json(messages);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+export const deleteMessage = async (req, res) => {
+  try {
+    const message = await Message.findById(req.params.id);
+    if (message.userId === req.user.id) {
+      await Message.findByIdAndDelete(req.params.id);
+      res.status(200).json("Message deleted successfully");
+    } else {
+      return res.status(403).json("You can only delete your own messages");
     }
   } catch (err) {
     res.status(500).json(err);
