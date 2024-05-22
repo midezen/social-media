a9fca3c1b684aea6dc890322c71db9cfde732742
midezen <victordiya90@gmail.com>
2024-05-22 15:16:24 +0100

    created editMessage function in Message controller

--- server/controllers/Message.js
+++ server/controllers/Message.js
@@ -12,6 +12,15 @@ export const createMessage = async (req, res) => {
 
 export const editMessage = async (req, res) => {
   try {
+    const message = await Message.findById(req.params.id);
+    if (message.userId === req.user.id) {
+      const editedMessage = await Message.findByIdAndUpdate(
+        req.params.id,
+        { $set: req.body },
+        { new: true }
+      );
+      res.status(200).json(editedMessage);
+    }
   } catch (err) {
     res.status(500).json(err);
   }
